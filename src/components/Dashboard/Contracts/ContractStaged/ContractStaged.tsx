2c91a784af72d71c71013786c2af093fde5bbe80
Uladzislau Tsybulsky <u.tsybulski@altoros.com>
2022-07-15 01:41:32 +0300

    Add header styling and layout

--- src/components/Dashboard/Contracts/ContractStaged/ContractStaged.tsx
+++ src/components/Dashboard/Contracts/ContractStaged/ContractStaged.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useCallback, Dispatch } from 'react'
-import { useParams, useHistory } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { getContractDetails } from 'src/api/contracts'
 import { ActionType, State, Action } from '../store/redux'
-import { ContractDetailsContainer } from './styles'
+import ContractStatusWidget from '../../../common/ContractStatusWidget/index'
+import { ContractStagedContainer, ContractStagedHeader } from './styles'
 
 interface IContractDetailsProps {
   state: State
@@ -11,7 +12,6 @@ interface IContractDetailsProps {
 
 const ContractStaged: React.FC<IContractDetailsProps> = ({ state, dispatch }: IContractDetailsProps): JSX.Element => {
   let { id } = useParams<{ id: string }>()
-  const history = useHistory()
   const { contractDetails } = state
 
   const fetchData = useCallback(async () => {
@@ -46,20 +46,120 @@ const ContractStaged: React.FC<IContractDetailsProps> = ({ state, dispatch }: IC
   }, [fetchData])
 
   return (
-    <ContractDetailsContainer>
+    <ContractStagedContainer>
       {!contractDetails ? (
         <div>Loading...</div>
       ) : (
-        <div>
-          <h5>Contract Number: {contractDetails.name}</h5>
-          <h5>Contract IPS {contractDetails.isp}</h5>
-          <h5>Contract Start Date {contractDetails.startDate}</h5>
-          <button type="button" onClick={history.goBack}>
-            Go Back
-          </button>
-        </div>
+
+        <ContractStagedHeader>
+          <div className='title'>
+            <div className='contract-number'>
+              <span className='icon icon-32 icon-ongoing icon-lighter-blue'></span>
+              <h5>{contractDetails.name}</h5>
+
+              { contractDetails.lta ? (
+                <div className='lta-number'>
+                  <span className='icon icon-24 icon-contract icon-light-grey'></span>
+                  <h5>{contractDetails.lta}</h5>
+                </div>
+              ) : ('') }
+
+            </div>
+
+            <span className='title-item'>
+              <span className='icon icon-24 icon-files icon-mid-grey'></span>
+              <p>Attachments</p>
+            </span>
+
+            <span className='title-item'>
+              <span className='icon icon-24 icon-network icon-mid-grey'></span>
+              <p>{contractDetails.isp}</p>
+            </span>
+            
+            
+          </div>
+
+          <div className='info'>
+
+            <button>
+              <div className='button-title'>
+                <h5>Schools</h5>
+                <small><b>{contractDetails.numberOfSchools}</b></small>
+              </div>
+
+              <div className='button-info'>
+                <div className='button-metric'>
+                  <span className='icon icon-20 icon-plug icon-mid-grey'></span>
+                  <small><b>
+                    95%
+                    {/* { contractDetails.connectionsMedian[0].median_value + contractDetails.connectionsMedian[0].unit } */}
+                  </b></small>
+                </div>
+
+                <div className='button-metric'>
+                  <span className='icon icon-20 icon-meter icon-mid-grey'></span>
+                  <small><b>200ms
+                  </b></small>
+                </div>
+
+                <div className='button-metric'>
+                  <span className='icon icon-20 icon-down-speed icon-mid-grey'></span>
+                  <small><b>100Mb/s
+                  </b></small>
+                </div>
+
+                <div className='button-metric'>
+                  <span className='icon icon-20 icon-up-speed icon-mid-grey'></span>
+                  <small><b>100Mb/s
+                  </b></small>
+                </div>
+              </div>
+
+              <div className='button-chart'>
+                <ContractStatusWidget 
+                  average={contractDetails.schoolsConnection.atLeastOneBellowAvg} 
+                  good={contractDetails.schoolsConnection.allEqualOrAboveAvg} 
+                  payments={60} />
+              </div>
+
+            </button>
+
+            <button>
+              <div className='button-title'>
+                <h5>Payments</h5>
+                <small><b>6</b></small>
+              </div>
+
+              <div className='button-info'>
+                <div className='button-metric'>
+                  <span className='icon icon-20 icon-coins icon-mid-grey'></span>
+                  <small><b>9000000</b></small>
+                  <small><b>/</b></small>
+                  <small><b>6000000</b></small>
+                </div>
+              </div>
+
+              <div className='button-chart'>
+                <ContractStatusWidget average={10} good={80} payments={60} />
+              </div>
+
+            </button>
+
+            <div className='dates'>
+              <span className='icon icon-24 icon-date icon-light-blue'></span>
+              <p>Start Date:</p>
+              <p><b>{new Date(contractDetails.startDate).toDateString()}</b></p>
+              <span className='icon icon-24 icon-date icon-light-blue'></span>
+              <p>End Date:</p>
+              <p><b>{new Date(contractDetails.endDate).toDateString()}</b></p>
+            </div>
+
+          </div>
+        </ContractStagedHeader>
       )}
-    </ContractDetailsContainer>
+
+
+    </ContractStagedContainer>
   )
 }
 
