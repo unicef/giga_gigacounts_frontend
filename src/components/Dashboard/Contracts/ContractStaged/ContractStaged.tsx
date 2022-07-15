b7e25d7d26f12dcd2fb0ce63d9edb44c4236d9b3
ancutadaniel <ancutadaniel@gmail.com>
2022-07-15 11:40:20 +0300

    update school widget

--- src/components/Dashboard/Contracts/ContractStaged/ContractStaged.tsx
+++ src/components/Dashboard/Contracts/ContractStaged/ContractStaged.tsx
@@ -18,24 +18,7 @@ const ContractStaged: React.FC<IContractDetailsProps> = ({ state, dispatch }: IC
     try {
       const response = await getContractDetails(id)
 
-      let formatStartDate: string = ''
-      let formatEndDate: string = ''
-
-      if (response.startDate.length > 0) {
-        formatStartDate = response.startDate.slice(0, 10)
-      }
-
-      if (response.endDate.length > 0) {
-        formatEndDate = response.endDate.slice(0, 10)
-      }
-
-      const formattedResponse = {
-        ...response,
-        startDate: formatStartDate,
-        endDate: formatEndDate,
-      }
-
-      dispatch({ type: ActionType.SET_CONTRACT_DETAILS, payload: formattedResponse })
+      dispatch({ type: ActionType.SET_CONTRACT_DETAILS, payload: response })
     } catch (error) {
       dispatch({ type: ActionType.SET_ERROR, payload: error })
     }
@@ -50,115 +33,127 @@ const ContractStaged: React.FC<IContractDetailsProps> = ({ state, dispatch }: IC
       {!contractDetails ? (
         <div>Loading...</div>
       ) : (
-
         <ContractStagedHeader>
-          <div className='title'>
-            <div className='contract-number'>
-              <span className='icon icon-32 icon-ongoing icon-lighter-blue'></span>
+          <div className="title">
+            <div className="contract-number">
+              <span className="icon icon-32 icon-ongoing icon-lighter-blue"></span>
               <h5>{contractDetails.name}</h5>
 
-              { contractDetails.lta ? (
-                <div className='lta-number'>
-                  <span className='icon icon-24 icon-contract icon-light-grey'></span>
+              {contractDetails.lta ? (
+                <div className="lta-number">
+                  <span className="icon icon-24 icon-contract icon-light-grey"></span>
                   <h5>{contractDetails.lta}</h5>
                 </div>
-              ) : ('') }
-
+              ) : (
+                ''
+              )}
             </div>
 
-            <span className='title-item'>
-              <span className='icon icon-24 icon-files icon-mid-grey'></span>
+            <span className="title-item">
+              <span className="icon icon-24 icon-files icon-mid-grey"></span>
               <p>Attachments</p>
             </span>
 
-            <span className='title-item'>
-              <span className='icon icon-24 icon-network icon-mid-grey'></span>
+            <span className="title-item">
+              <span className="icon icon-24 icon-network icon-mid-grey"></span>
               <p>{contractDetails.isp}</p>
             </span>
-            
-            
           </div>
 
-          <div className='info'>
-
+          <div className="info">
             <button>
-              <div className='button-title'>
+              <div className="button-title">
                 <h5>Schools</h5>
-                <small><b>{contractDetails.numberOfSchools}</b></small>
+                <small>
+                  <b>{contractDetails.numberOfSchools}</b>
+                </small>
               </div>
 
-              <div className='button-info'>
-                <div className='button-metric'>
-                  <span className='icon icon-20 icon-plug icon-mid-grey'></span>
-                  <small><b>
-                    95%
-                    {/* { contractDetails.connectionsMedian[0].median_value + contractDetails.connectionsMedian[0].unit } */}
-                  </b></small>
-                </div>
+              <div className="button-info">
+                {contractDetails.connectionsMedian &&
+                  contractDetails.connectionsMedian.map((item, i) => {
+                    let icon
+                    switch (item.metric_id) {
+                      case 1:
+                        icon = <span className={`icon icon-20 icon-plug icon-mid-grey`}></span>
+                        break
+                      case 2:
+                        icon = <span className={`icon icon-20 icon-meter icon-mid-grey`}></span>
+                        break
+                      case 3:
+                        icon = <span className={`icon icon-20 icon-down-speed icon-mid-grey`}></span>
+                        break
+                      case 4:
+                        icon = <span className={`icon icon-20 icon-up-speed icon-mid-grey`}></span>
+                        break
 
-                <div className='button-metric'>
-                  <span className='icon icon-20 icon-meter icon-mid-grey'></span>
-                  <small><b>200ms
-                  </b></small>
-                </div>
+                      default:
+                        break
+                    }
 
-                <div className='button-metric'>
-                  <span className='icon icon-20 icon-down-speed icon-mid-grey'></span>
-                  <small><b>100Mb/s
-                  </b></small>
-                </div>
-
-                <div className='button-metric'>
-                  <span className='icon icon-20 icon-up-speed icon-mid-grey'></span>
-                  <small><b>100Mb/s
-                  </b></small>
-                </div>
+                    return (
+                      <div key={i} className="button-metric">
+                        {icon}
+                        <small>
+                          <b style={{ textTransform: 'none' }}>{item.median_value + item.unit}</b>
+                        </small>
+                      </div>
+                    )
+                  })}
               </div>
 
-              <div className='button-chart'>
-                <ContractStatusWidget 
-                  average={contractDetails.schoolsConnection.atLeastOneBellowAvg} 
-                  good={contractDetails.schoolsConnection.allEqualOrAboveAvg} 
-                  payments={60} />
+              <div className="button-chart">
+                <ContractStatusWidget
+                  average={contractDetails.schoolsConnection.atLeastOneBellowAvg}
+                  good={contractDetails.schoolsConnection.allEqualOrAboveAvg}
+                  payments={60}
+                />
               </div>
-
             </button>
 
             <button>
-              <div className='button-title'>
+              <div className="button-title">
                 <h5>Payments</h5>
-                <small><b>6</b></small>
+                <small>
+                  <b>6</b>
+                </small>
               </div>
 
-              <div className='button-info'>
-                <div className='button-metric'>
-                  <span className='icon icon-20 icon-coins icon-mid-grey'></span>
-                  <small><b>9000000</b></small>
-                  <small><b>/</b></small>
-                  <small><b>6000000</b></small>
+              <div className="button-info">
+                <div className="button-metric">
+                  <span className="icon icon-20 icon-coins icon-mid-grey"></span>
+                  <small>
+                    <b>9000000</b>
+                  </small>
+                  <small>
+                    <b>/</b>
+                  </small>
+                  <small>
+                    <b>6000000</b>
+                  </small>
                 </div>
               </div>
 
-              <div className='button-chart'>
+              <div className="button-chart">
                 <ContractStatusWidget average={10} good={80} payments={60} />
               </div>
-
             </button>
 
-            <div className='dates'>
-              <span className='icon icon-24 icon-date icon-light-blue'></span>
+            <div className="dates">
+              <span className="icon icon-24 icon-date icon-light-blue"></span>
               <p>Start Date:</p>
-              <p><b>{new Date(contractDetails.startDate).toDateString()}</b></p>
-              <span className='icon icon-24 icon-date icon-light-blue'></span>
+              <p>
+                <b>{new Date(contractDetails.startDate).toDateString()}</b>
+              </p>
+              <span className="icon icon-24 icon-date icon-light-blue"></span>
               <p>End Date:</p>
-              <p><b>{new Date(contractDetails.endDate).toDateString()}</b></p>
+              <p>
+                <b>{new Date(contractDetails.endDate).toDateString()}</b>
+              </p>
             </div>
-
           </div>
         </ContractStagedHeader>
       )}
-
-
     </ContractStagedContainer>
   )
 }
