54c540a2ad984c90b71045fc7c8700828588242f
Uladzislau Tsybulsky <vladtsm@gmail.com>
2022-07-25 21:01:03 +0300

    Add dialog component

---
+++ src/components/common/Dialog/Dialog.tsx
@@ -0,0 +1,55 @@
+import { DialogContainer } from './styles'
+
+interface DialogProps {
+  show?: boolean
+  type?: string // message, warning
+  message?: string
+  acceptLabel?: string
+  rejectLabel?: string
+  onAccepted?: () => void
+  onRejected?: () => void
+}
+
+const styles = (value: string) => {
+    if ( value === 'warning') {
+        return { 
+            "icon" : "icon icon-60 icon-error icon-red-on-blue", 
+            "button" : "btn-red" 
+            }
+    }
+    else {
+        return { 
+            "icon" : "icon icon-60 icon-expired icon-light-blue", 
+            "button" : "btn-blue" 
+        }
+    }
+  }
+
+const Dialog: React.FC<DialogProps> = ({
+  show = false,
+  type = 'message',
+  message = 'Do you really want to perform this action',
+  acceptLabel = 'Ok',
+  rejectLabel = 'Cancel',
+  onAccepted,
+  onRejected
+}: DialogProps): JSX.Element => {
+  return (
+    <>
+      {show && (
+        <DialogContainer>
+          <div className='dialog'>
+              <span className={styles(type).icon}></span>
+              <p>{message}</p>
+              <div className='cta'>
+                  <button className={styles(type).button} onClick={onAccepted}> {acceptLabel} </button>
+                  <button className='btn-transparent-grey' onClick={onRejected}> {rejectLabel} </button>
+              </div>
+          </div>
+        </DialogContainer>
+      )}
+    </>
+  )
+}
+
+export default Dialog
