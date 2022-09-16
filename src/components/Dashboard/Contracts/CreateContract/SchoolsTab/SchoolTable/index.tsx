5f6bd8c0b8cb1b416bf436b480de5520a9164deb
Alaa Saleh <photonite@users.noreply.github.com>
2022-09-16 16:30:41 -0300

    feat(SchoolsTab): show number of selected schools (#149)

--- src/components/Dashboard/Contracts/CreateContract/SchoolsTab/SchoolTable/index.tsx
+++ src/components/Dashboard/Contracts/CreateContract/SchoolsTab/SchoolTable/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { ISchool } from 'src/api/school'
 import {
   SchoolsTableHeader,
@@ -10,6 +10,7 @@ import {
   SchoolIDContainer,
   MainContainer,
   SchoolTableContent,
+  SchoolsNumberBadge,
 } from './styles'
 import { sortSchools } from './utils'
 
@@ -20,6 +21,8 @@ interface SchoolTableProps {
 }
 
 const SchoolTable = ({ schools, onSelect, selectedSchoolsIds }: SchoolTableProps) => {
+  const tableRef = useRef<HTMLDivElement>(null)
+  const [scrollTop, setScrollTop] = useState<number | undefined>(undefined)
   const sortedSchools = useMemo(
     () => schools.map((school) => ({ ...school, selected: selectedSchoolsIds.includes(school.id) })).sort(sortSchools),
     [schools, selectedSchoolsIds],
@@ -27,25 +30,36 @@ const SchoolTable = ({ schools, onSelect, selectedSchoolsIds }: SchoolTableProps
 
   const onButtonSelect = useCallback(
     (id: string) => () => {
+      setScrollTop(tableRef.current?.scrollTop)
+
       onSelect(id)
     },
     [onSelect],
   )
 
+  useEffect(() => {
+    if (scrollTop !== undefined && tableRef.current !== null) {
+      tableRef.current.scrollTop = scrollTop
+      setScrollTop(undefined)
+    }
+  }, [scrollTop])
+
   return (
     <MainContainer>
       <SchoolsTableHeader>
+        <SchoolsNumberBadge title="Number of selected schools">{selectedSchoolsIds.length}</SchoolsNumberBadge>
         <NameHeaderLabel>School Name</NameHeaderLabel>
         <IdHeaderLabel>ID</IdHeaderLabel>
       </SchoolsTableHeader>
-      <SchoolTableContent>
+      <SchoolTableContent ref={tableRef}>
         {sortedSchools.map((school) => (
-          <TableRow key={school.id}>
+          <TableRow key={school.id} htmlFor={`school${school.id}`}>
             <SelectButton
               type="checkbox"
               onChange={onButtonSelect(school.id)}
               checked={school.selected}
               value={school.id}
+              id={`school${school.id}`}
             />
             <SchoolNameContainer>{school.name}</SchoolNameContainer>
             <SchoolIDContainer>{school.external_id}</SchoolIDContainer>
--- src/components/Dashboard/Contracts/CreateContract/SchoolsTab/SchoolTable/styles.ts
+++ src/components/Dashboard/Contracts/CreateContract/SchoolsTab/SchoolTable/styles.ts
@@ -10,12 +10,11 @@ export const MainContainer = styled.div`
 `
 
 export const SchoolsTableHeader = styled.div`
-  display: flex;
-  flex-direction: row;
+  display: grid;
+  grid-template-columns: minmax(20px, 8%) 1fr minmax(62px, 16%);
   align-items: baseline;
   width: 100%;
-  justify-content: space-between;
-  padding: 8px 16px;
+  padding: 8px 16px 8px 0;
   gap: 8px;
   border-bottom: 1px solid;
   border-bottom-color: rgba(0, 0, 0, 0.1);
@@ -33,13 +32,22 @@ export const NameHeaderLabel = styled.p`
   color: var(--color-dark-grey);
 `
 
+export const SchoolsNumberBadge = styled.span`
+  color: var(--color-light-blue);
+  min-width: 16px;
+  display: inline-block;
+  text-align: center;
+  cursor: default;
+  user-select: none;
+`
+
 export const IdHeaderLabel = styled.p`
   font-family: 'Open Sans';
   font-style: normal;
   font-weight: 600;
   font-size: 14px;
   letter-spacing: 0.003em;
-  align-self: flex-end;
+  text-align: right;
   color: var(--color-dark-grey);
   padding-right: 14px;
 `
@@ -63,13 +71,14 @@ export const SchoolTableContent = styled.div`
   }
 `
 
-export const TableRow = styled.div`
+export const TableRow = styled.label`
   display: inline-grid;
   width: 100%;
-  grid-template-columns: 5% 85% 10%;
+  grid-template-columns: minmax(20px, 8%) 1fr minmax(62px, 16%);
   justify-items: start;
   align-items: center;
-  padding: 8px 16px;
+  padding: 8px 16px 8px 0;
+  cursor: pointer;
 `
 
 export const SelectButton = styled.input`
